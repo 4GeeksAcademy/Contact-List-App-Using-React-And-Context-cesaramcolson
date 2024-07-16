2274e6e44a98549e63d12fe587f2a28def492323
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-07-16 23:56:49 +0000

    adding some styling to the contact cardand the navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<div className="mx-4">
-				<Link to="/add">
-					<button className="btn btn-success">Add new contact</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <div className="container-fluid justify-content-end mx-4">
+        <Link to="/add" className="text-decoration-none">
+          <button className="btn btn-success px-3">Add new contact</button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
